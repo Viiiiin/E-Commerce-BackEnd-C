02e1f7f8e4406b59612182e073e2bf8e65b6a6e9
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-03-08 19:28:54 +0100

    Dati i permessi

--- db-scripts/grant.sql
+++ db-scripts/grant.sql
@@ -7,7 +7,11 @@ GRANT ALL PRIVILEGES ON DATABASE :dbname to :username ;
 
 ALTER TABLE TimeVar OWNER TO :username ;
 ALTER TABLE LogTable OWNER TO :username ;
-
+ALTER TABLE Produttore OWNER TO :username ;
+ALTER TABLE Costumer OWNER TO :username ;
+ALTER TABLE Trasportatore OWNER TO :username ;
+ALTER TABLE Prodotto OWNER TO :username ;
+ALTER TABLE Acquisto OWNER TO :username ;
 -- grant all privileges on all tables in schema public to :username ;
 -- grant all privileges on all sequences in schema public to :username ;
 
