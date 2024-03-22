c9f0c99e5bb585b281948190ebef966adbb1c13d
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-03-22 11:57:15 +0100

    Versione con While

--- Client-Produttore/src/main.cpp
+++ Client-Produttore/src/main.cpp
@@ -1,12 +1,21 @@
 
 #include "produttore.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
 int main(){
-
+    int i=1;
+    int x=0;
     Produttore prod(4);
-    char *nome = "Computer";
-    char *descrizione = "Un computer";
-    prod.inserisciProdotto(nome,descrizione);
-
+    char* nome[] = {"Computer","Televisore","Mouse","Tastiera"};
+    char* descrizione[] = {"Un computer","Un televisore", "Un Mouse","Una Tastiera"};
+    while(x<100){
+        i= (i+5^2+7)%4;
+        prod.inserisciProdotto(nome[i],descrizione[i]);
+        cout << "Ha inserito un prodotto: ";
+        cout << x << endl;
+        x++;
+    }
     return 0;
 }
