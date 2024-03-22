4e6f07d71d52d673aa1929b99c12a8cfeda167cb
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-03-22 11:59:28 +0100

    funzionante v1.0

--- Server-Produttore/src/main.cpp
+++ Server-Produttore/src/main.cpp
@@ -7,17 +7,22 @@ int main(){
     int i=0;
     int j=0;
     int block = 1000000000;
-
+    int read_counter=0;
+    int send_counter=0;
+    Cmd_Reply cmd_reply;
 
     ServerP server("serverProd");
     cout << "Il Server dei Produttori sta ascoltando..." << endl;
     while(1){
 
-        char *cmd = server.readCommandRedis(block);
-
-        if (cmd=="Inserisci"){
-                server.inserisciProdotto(block);
+        cmd_reply = server.readCommandRedis(block);
+        cout << cmd_reply.cmd << endl;
+        if (cmd_reply.cmd =="Inserisci"){
+            cout<<"Inserendo il prodotto nel database..."<< endl;
+            server.inserisciProdotto(block,cmd_reply.reply);
+            cout<<"Prodotto inserito con successo"<< endl;
         }
+        read_counter++;
     }
 
     return 0;
