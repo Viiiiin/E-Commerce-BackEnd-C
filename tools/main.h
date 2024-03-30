f71bc6f583289b80a5716e17e6b27c088650b9c5
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-03-30 17:27:25 +0100

    aggiunto

---
+++ tools/main.h
@@ -0,0 +1,34 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stddef.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <gsl/gsl_fit.h>
+#include <cassert>
+#include <cerrno>
+
+
+#define DEBUG 1000
+
+#define HORIZON 10  // TICKS
+
+
+int msleep(long msec);
+int micro_sleep(long usec);
+int long get_nanos(void);
+long int get_day_nanos(char* buf);
+long int nanos2day(char* buf, long int nanosec);
+
+
+
+using namespace std;
+
+#endif
