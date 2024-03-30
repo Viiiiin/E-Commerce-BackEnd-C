6c8d4c7f2f67e4d9e111c792374342aa8942db41
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-03-30 17:25:05 +0100

    Multiprocesso

--- Client-Produttore/src/main.cpp
+++ Client-Produttore/src/main.cpp
@@ -1,21 +1,51 @@
-
 #include "produttore.h"
 #include <string>
 #include <iostream>
+#include <unistd.h> // Libreria per la fork()
+#include "../../tools/msleep.cpp"
+#include <math.h>
+#include <sys/wait.h> // Libreria per la wait()
 using namespace std;
 
-int main(){
-    int i=1;
-    int x=0;
-    Produttore prod(4);
+void produttoreProcesso(Produttore& prod) {
     char* nome[] = {"Computer","Televisore","Mouse","Tastiera"};
     char* descrizione[] = {"Un computer","Un televisore", "Un Mouse","Una Tastiera"};
-    while(x<100){
-        i= (i+5^2+7)%4;
-        prod.inserisciProdotto(nome[i],descrizione[i]);
-        cout << "Ha inserito un prodotto: ";
-        cout << x << endl;
+
+    int x = 0;
+    long time = 1;
+    pid_t pid;
+    while (x < 1000) {
+        msleep(time);
+        int i = rand() % 4;
+        prod.inserisciProdotto(nome[i], descrizione[i]);
+        pid = getpid();
+        cout << pid;
+        cout << " Ha inserito un prodotto: ";
         x++;
+        cout << x << endl;
     }
+}
+
+int main() {
+    int num_produttori = 1000;
+    // Creazione di processi figlio
+    for (int i = 0; i < num_produttori; ++i) {
+        Produttore prod(4);
+        pid_t pid = fork();
+
+        if (pid == 0) { // Processo figlio
+            produttoreProcesso(prod);
+            exit(0); // Termina il processo figlio dopo aver completato il lavoro
+        } else if (pid < 0) { // Errore nella creazione del processo figlio
+            cerr << "Errore nella fork()" << endl;
+            exit(1);
+        }
+    }
+
+    // Attendi che tutti i processi figlio terminino prima di terminare il processo principale
+    int status;
+    pid_t pid;
+    while ((pid = wait(&status)) > 0);
+
     return 0;
 }
