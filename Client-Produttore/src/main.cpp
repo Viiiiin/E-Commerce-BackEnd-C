9872c3d977bed1ecb63ecd601bbae55bb7f5abd7
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-04 17:45:46 +0200

    Aggiunti struct prodotto

--- Client-Produttore/src/main.cpp
+++ Client-Produttore/src/main.cpp
@@ -7,34 +7,58 @@
 #include <sys/wait.h> // Libreria per la wait()
 using namespace std;
 
-void produttoreProcesso(Produttore& prod) {
-    char* nome[] = {"Computer","Televisore","Mouse","Tastiera"};
-    char* descrizione[] = {"Un computer","Un televisore", "Un Mouse","Una Tastiera"};
+void produttoreProcesso(Produttore& prod, long time) {
+       Prodotto prodotti[] = {
+        {"Maglietta", "Maglietta di cotone blu con logo stampato", {"EUR", 19}},
+        {"Pantaloni", "Pantaloni jeans regular fit", {"EUR", 39}},
+        {"Scarpe", "Scarpe sportive leggere e traspiranti", {"EUR", 59}},
+        {"Cappello", "Cappello vintage anni 60", {"USD", 25}}
+        };
 
+    int res;
     int x = 0;
-    long time = 1;
     pid_t pid;
     while (x < 1000) {
         msleep(time);
         int i = rand() % 4;
-        prod.inserisciProdotto(nome[i], descrizione[i]);
+        res = prod.inserisciProdotto(prodotti[i].nome, prodotti[i].descrizione,prodotti[i].prezzo);
         pid = getpid();
         cout << pid;
+        if (res==0){
         cout << " Ha inserito un prodotto: ";
+        }
+        else {
+            cout << " Inserimento prodotto non riuscito n: ";
+        }
         x++;
         cout << x << endl;
     }
 }
 
-int main() {
-    int num_produttori = 1000;
+int main(int argc,char *argv[]){
+	
+	//Dichiarazione e inizializzazione delle variabili
+	int numero_produttori= 0;
+	long time_to_sleep= 0;
+    char c;
+	
+    while ((c = getopt(argc, argv, "p:s:")) != -1) {
+        switch (c) {
+            case 'p':    /* numero produttori */
+              numero_produttori = atoi(optarg);
+              break;
+            case 't':    /* tempo di dormita */
+              time_to_sleep = atoi(optarg);
+              break;
+        }
+    }
     // Creazione di processi figlio
-    for (int i = 0; i < num_produttori; ++i) {
-        Produttore prod(4);
+    for (int i = 1; i <= numero_produttori; ++i) {
+        Produttore prod(i);
         pid_t pid = fork();
 
         if (pid == 0) { // Processo figlio
-            produttoreProcesso(prod);
+            produttoreProcesso(prod,time_to_sleep);
             exit(0); // Termina il processo figlio dopo aver completato il lavoro
         } else if (pid < 0) { // Errore nella creazione del processo figlio
             cerr << "Errore nella fork()" << endl;
