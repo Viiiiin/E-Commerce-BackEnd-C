18b563e7391a5cc6844a344e1a44a5775889a31c
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-04 17:45:55 +0200

    Aggiunti struct prodotto

--- Client-Produttore/src/produttore.cpp
+++ Client-Produttore/src/produttore.cpp
@@ -15,12 +15,15 @@ Produttore::Produttore(int id)
 
 
 
-void Produttore::inserisciProdotto(char *nome, char *descrizione){
+int Produttore::inserisciProdotto(const char *nome,const char *descrizione, Prezzo prezzo){
     char key0[100];
     char key1[100];
     char key2[100];
     char key3[100]; 
     char key4[100];
+    char result[100];
+    int block = 10000000;
+    int k,i;
     redisReply *reply;
 
     sprintf(key0,"comando");
@@ -31,10 +34,25 @@ void Produttore::inserisciProdotto(char *nome, char *descrizione){
 
    
 
-    reply= RedisCommand(this->c2r, "XADD %s * %s %s %s %s %s %s",this->WRITE_STREAM, key0, "Inserisci", key1, nome, key2, descrizione);
+    reply= RedisCommand(this->c2r, "XADD %s * %s %s %s %s %s %s %s %d %s %s %s %d",this->WRITE_STREAM, key0, "Inserisci", key1, nome, key2, descrizione,"produttore",this->id,"valuta",prezzo.valuta ,"prezzo",prezzo.prezzo);
 
     assertReplyType(this->c2r,reply,REDIS_REPLY_STRING);
 
     freeReplyObject(reply);
 
+    reply = RedisCommand(this->c2r, "XREADGROUP GROUP diameter %d BLOCK %d COUNT 1 NOACK STREAMS %s >", this->id, block, this->READ_STREAM);
+    
+    assertReply(this->c2r, reply);
+
+    k = ReadNumStreams(reply) -1; // prendo l'ultima stream inviata
+    
+    i = ReadStreamNumMsg(reply, k) -1; // ultimo messaggio dell'ultima stream
+
+    ReadStreamMsgVal(reply, k, i, 1, result);
+    freeReplyObject(reply);
+
+    if (strcmp(result, "Aggiunto") != 0){ 
+        return 1;
+    }
+    return 0;
 }
