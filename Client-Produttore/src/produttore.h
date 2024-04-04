74d705361d12ed658b864c0142e0466dd9abc5c7
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-04 17:45:59 +0200

    Aggiunti struct prodotto

--- Client-Produttore/src/produttore.h
+++ Client-Produttore/src/produttore.h
@@ -2,13 +2,28 @@
 #define produttore_h
 
 #include "con2redis.h"
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+struct Prezzo {
+    char valuta[4]; // Campo valuta di 3 char + terminatore null
+    int prezzo;
+};
+
+struct Prodotto {
+    const char* nome;
+    const char* descrizione;
+    Prezzo prezzo;
+};
 
 class Produttore {
     public: 
 
         Produttore(const int id);
 
-        void inserisciProdotto(char *nome, char *descrizione);
+        int inserisciProdotto(const char *nome, const char *descrizione,Prezzo prezzo);
     private: 
         int id;
         redisContext *c2r;
