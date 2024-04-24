164211c8f1a61422385fe5723195e6c4ba5d589a
projjettto <chiarello.2000457@studenti.uniroma1.it>
2024-04-24 18:01:02 +0200

    assegnaTrasportatore

--- db-scripts/schema.sql
+++ db-scripts/schema.sql
@@ -1,3 +1,4 @@
+
 -- database already exists
 
 \c :dbname 
@@ -67,17 +68,16 @@ CREATE TABLE IF NOT EXISTS Costumer(
 	email SmallVar NOT NULL,
 	indirizzo Indirizzo NOT NULL
 );
-	
-	
+		
 CREATE TABLE IF NOT EXISTS Produttore(
 	id serial PRIMARY KEY,
-	ragioneSociale MediumVar NOT NULL,
+	nome MediumVar NOT NULL,
 	sede indirizzo NOT NULL 
 );
 	
 CREATE TABLE IF NOT EXISTS Trasportatore(
 	id serial PRIMARY KEY,
-	ragioneSociale MediumVar NOT NULL,
+	nome MediumVar NOT NULL,
 	sede indirizzo NOT NULL 
 );
 	
@@ -112,7 +112,7 @@ INSERT INTO nomefun (nome) VALUES
     ('Vendita');
 
 
-INSERT INTO Trasportatore (ragioneSociale, sede) VALUES 
+INSERT INTO Trasportatore (nome, sede) VALUES 
     ('Azienda di Trasporti ABC', ('Via Roma', '123', '00100')),
     ('Trasporti XYZ S.p.A.', ('Via Milano', '456', '00200')),
     ('Logistica 123', ('Corso Napoli', '789', '00300'));
@@ -125,7 +125,7 @@ VALUES
     ('Anna', 'Verdi', 'anna.verdi@example.com', ('Corso Napoli', '789', '00300'));
 
 
-INSERT INTO Produttore (ragioneSociale, sede) 
+INSERT INTO Produttore (nome, sede) 
 VALUES 
     ('Azienda Tessile S.r.l.', ('Via Garibaldi', '10', '20100')),
     ('Calzaturificio Italiano S.p.A.', ('Corso Italia', '20', '50123')),
