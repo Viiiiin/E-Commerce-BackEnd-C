84e24a98f6295f4fc9dc11e329dd9200dc06cf3f
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-27 12:41:54 +0200

    Sviluppato

--- Trasportatore/src/trasportatore.cpp
+++ Trasportatore/src/trasportatore.cpp
@@ -6,14 +6,51 @@ Trasportatore:: Trasportatore (
 )
 
 { 
+    this->ragioneSociale= ragioneSociale;
+    this->id=id;
     this->READ_STREAM = "stream_trasportatore_1";
     this->WRITE_STREAM = "stream_trasportatore_2";
-    //int read_counter = 0;
-    //int send_counter = 0;
     this->c2r = redisConnect("localhost", 6379);
-    
+    redisReply *reply;
+
+   
+   
     /* Create streams/groups */
     initStreams(this->c2r, this->READ_STREAM);
-    initStreams(this->c2r, this->WRITE_STREAM);  
+    initStreams(this->c2r, this->WRITE_STREAM);
 };
 
+string Trasportatore::consegnaProdotto(const int idAcquisto){
+    char key0[100];
+    char key1[100];
+    char key2[100];
+    char result[100];
+    int block = 10000000;
+    int k,i;
+    redisReply *reply;
+
+    sprintf(key0,"comando");
+    sprintf(key1, "nome");
+    sprintf(key2,"Acquisto");
+
+   
+
+    reply= RedisCommand(this->c2r, "XADD %s * %s %s %s %d %s %d",this->WRITE_STREAM, key0, "Consegna", key1, this->id, key2,idAcquisto);
+
+    assertReplyType(this->c2r,reply,REDIS_REPLY_STRING);
+
+    freeReplyObject(reply);
+
+    reply = RedisCommand(this->c2r, "XREADGROUP GROUP diameter %d BLOCK %d COUNT 1 NOACK STREAMS %s >", this->id, block, this->READ_STREAM);
+    
+    assertReply(this->c2r, reply);
+
+    k = ReadNumStreams(reply) -1; // prendo l'ultima stream inviata
+    
+    i = ReadStreamNumMsg(reply, k) -1; // ultimo messaggio dell'ultima stream
+
+    ReadStreamMsgVal(reply, k, i, 1, result);
+    freeReplyObject(reply);
+    return result;
+   
+}
