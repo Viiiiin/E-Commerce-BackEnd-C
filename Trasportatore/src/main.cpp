37ab2ee133a46ed594a743f69a08f80d7e874f96
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-27 12:41:45 +0200

    Sviluppato

--- Trasportatore/src/main.cpp
+++ Trasportatore/src/main.cpp
@@ -1,9 +1,21 @@
 #include "trasportatore.h"
+#include <string>
 #include <iostream>
 
+using namespace std;
 
-int main(){
-    Trasportatore tra(1,"HEY");
-    std::cout << "CREATO" << std::endl;
-    return 0;
+int main(){    
+    string res;
+    int i=1;
+    int x=0;
+    Trasportatore trasp(1, "BARTOLINI");
+    int prodotto[] = {1,2,3,4,5,6};
+    while(x<10){
+        i = rand() % 6;
+        res = trasp.consegnaProdotto(i);
+        cout << res << endl;
+        x++;
+        cout << x << endl;
     }
+    return 0;
+}
