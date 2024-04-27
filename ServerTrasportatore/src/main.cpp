e0db5c6c82f71abc178ae29d8f852e1a72bc3e76
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-27 12:40:08 +0200

    Sviluppato

--- ServerTrasportatore/src/main.cpp
+++ ServerTrasportatore/src/main.cpp
@@ -1,9 +1,27 @@
 #include "serverT.h"
+#include <string>
 #include <iostream>
-
+using namespace std;
 
 int main(){
-    ServerT tra("HEY");
-    std::cout << "CREATO" << std::endl;
-    return 0;
+   
+    int block = 1000000000;
+    int count=0;
+    Cmd_Reply cmd_reply;
+
+    ServerT server("serverTrasportatori");
+    cout << "Il Server dei Trasportatori sta ascoltando..." << endl;
+    while(1){
+        cmd_reply = server.readCommandRedis(block);
+        // cout << cmd_reply.cmd;
+        if (cmd_reply.cmd =="Consegna"){
+            
+            server.consegnaProdotto(block,cmd_reply.reply);
+            cout<<"Operazione completata: ";
+        }
+        count++;
+        cout<<count<<endl;
     }
+
+    return 0;
+}
