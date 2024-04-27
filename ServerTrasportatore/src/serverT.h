1cf6ef2712dcc5b3247c26d2518071970bc1e526
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-04-27 12:40:20 +0200

    Sviluppato

--- ServerTrasportatore/src/serverT.h
+++ ServerTrasportatore/src/serverT.h
@@ -1,19 +1,30 @@
 #ifndef serverT_h
 #define serverT_h
 
+
 #include "con2redis.h"
 #include "pgsql.h" // Include the header file for con2DB
+#include <string>
+#include <iostream>
 #include "../../tools/src/main.h"
 
+using namespace std;
+
+struct Cmd_Reply{
+    string cmd;
+    redisReply *reply;
+};
 class ServerT {
 public: 
     ServerT(const char *nome);
 
-    char *readCommandRedis(int block);
+    Cmd_Reply readCommandRedis(int block);
+    
+    void consegnaProdotto(int block,redisReply *reply);
 
 private: 
     redisContext *c2r;
-    Con2DB *db; // Declare a member for con2DB
+    Con2DB db; // Declare a member for con2DB
     const char *nome;
     const char *WRITE_STREAM;
     const char *READ_STREAM;
