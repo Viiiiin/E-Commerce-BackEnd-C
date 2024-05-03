385c975bb18bec9922e1f8865777a3ac9af9778e
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-05-03 16:00:30 +0200

    Aggiunto logger

--- Server-Produttore/src/main.cpp
+++ Server-Produttore/src/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(){
    
     int block = 1000000000;
-    int count=0;
     Cmd_Reply cmd_reply;
 
     ServerP server("serverProd");
@@ -14,18 +13,17 @@ int main(){
     while(1){
         cmd_reply = server.readCommandRedis(block);
         // cout << cmd_reply.cmd;
+        cout<< cmd_reply.cmd;
         if (cmd_reply.cmd =="Inserisci"){
             
             server.inserisciProdotto(block,cmd_reply.reply);
-            cout<<"Prodotto inserito con successo: ";
+           
         }
          if (cmd_reply.cmd =="Rimuovi"){
-            
             server.rimuoviProdotto(block,cmd_reply.reply);
-            cout<<"Prodotto rimosso con successo: ";
+        
         }
-        count++;
-        cout<<count<<endl;
+        
     }
 
     return 0;
