81bd4628ad3c050e504ac528552971421bd6e97d
projjettto <chiarello.2000457@studenti.uniroma1.it>
2024-05-08 14:30:27 +0200

    Aggiunta atoi

--- Costumer/src/main.cpp
+++ Costumer/src/main.cpp
@@ -11,13 +11,12 @@ int main(){
     int i=1;
     int x=0;
     Costumer cost1 (1, "Mario", "Rossi");
-    int prodotto1[] = {1,2,3,4,100,101,102};
+    int prodotto1[] = {1,2,3,4,5,6,7,444,555,777,888};
     while(x<100){
         // Generatore di numeri casuali
         std::random_device rd;
         std::mt19937 gen(rd());
         
-        // Distribuzione uniforme tra 1 e 100
         std::uniform_int_distribution<> dis(0, 6);
         
         // Estrazione di un numero casuale
@@ -28,13 +27,12 @@ int main(){
         cout << x << endl;
     }
     Costumer cost2 (2, "Luigi", "Bianchi");
-    int prodotto2[] = {5,6,7,8,9,10,11,12};
+    int prodotto2[] = {8,9,10,11,12,13,14,000,333,222};
     while(x<200){
         // Generatore di numeri casuali
         std::random_device rd;
         std::mt19937 gen(rd());
         
-        // Distribuzione uniforme tra 1 e 100
         std::uniform_int_distribution<> dis(0, 7);
         
         // Estrazione di un numero casuale
