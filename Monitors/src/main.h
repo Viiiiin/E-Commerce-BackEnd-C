cc707ea76dd4316d8b74ef176ce26612bdeee1fb
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-05-20 12:12:19 +0200

    Aggiunto

---
+++ Monitors/src/main.h
@@ -0,0 +1,16 @@
+#include "../../con2db/pgsql.h"
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include "../../tools/src/main.h"
+
+using namespace std;
+
+void monitorAttivita();
+
+void monitorFunzionalita();
+
+void monitorPrestazioni();
+
+void monitorScalabilita();
