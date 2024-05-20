1908659c89ad9aa251cb882764b833c8f1371c0b
projjettto <chiarello.2000457@studenti.uniroma1.it>
2024-05-20 13:26:19 +0200

    Non verificato

---
+++ Monitors/src/monitor-consegne.cpp
@@ -0,0 +1,34 @@
+#include "main.h"
+
+using namespace std;
+
+void monitorConsegne(){
+    Con2DB db("localhost", "5432", "ecommerce", "47002", "db_ecommerce");
+    char sqlcmd[1000]; 
+    PGresult *res;
+
+    sprintf(sqlcmd, "SELECT prodotto FROM Acquisto WHERE consegnato = false AND istante > DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 DAY);");
+
+    // Esecuzione della query e controllo del risultato
+    res = db.ExecSQLtuples(sqlcmd);
+    if (res != NULL && PQntuples(res) > 0) {        
+        int numProdotti = PQntuples(res);
+        long *listaProdotti = new long[numProdotti]; // Array dinamico per memorizzare i prodotti
+
+        // Itera sui risultati e memorizza i prodotti nell'array
+        for (int i = 0; i < numProdotti; ++i) {
+            listaProdotti[i] = strtol(PQgetvalue(res, i, PQfnumber(res, "prodotto")), nullptr, 10);
+        }
+
+        // Stampa i prodotti memorizzati nell'array
+        cout << "Elenco di prodotti:" << endl;
+        for (int i = 0; i < numProdotti; ++i) {
+            cout << listaProdotti[i] << endl;
+        }
+
+        delete[] listaProdotti; // Liberare la memoria allocata per l'array
+
+        PQclear(res);
+
+    }
+}
