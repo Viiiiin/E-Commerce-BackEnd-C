2e1c6f792d52dbf17751fb8299218785e01a8e10
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-05-20 12:11:31 +0200

    Multiprocesso con flag"

--- Trasportatore/src/main.cpp
+++ Trasportatore/src/main.cpp
@@ -40,3 +40,80 @@ int main(){
     return 0;
 
 }
+
+#include "trasportatore.h"
+#include <string>
+#include <iostream>
+#include <unistd.h> // Libreria per la fork()
+#include "../../tools/src/main.h"
+#include <math.h>
+#include <time.h>
+#include <random>
+#include <sys/wait.h> // Libreria per la wait()
+using namespace std;
+
+void trasportatoreProcess(Trasportatore& trasp, long time, int numero_operazioni) {      
+    int x = 0;
+    int i = 0;
+    string res;
+    while (x < numero_operazioni) {
+        msleep(time);
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, numero_operazioni);
+        
+        // Estrazione di un numero casuale
+        i = dis(gen);
+        res = trasp.consegnaProdotto(i);
+        x++;
+        cout << res <<": "<< x << endl;
+       
+    }
+}
+
+
+
+int main(int argc,char *argv[]){
+	
+	//Dichiarazione e inizializzazione delle variabili
+	int numero_trasportatori = 0;
+	long time_to_sleep = 0;
+    int numero_operazioni = 0;
+    char c;
+	
+    while ((c = getopt(argc, argv, "r:t:n:")) != -1) {
+        switch (c) {
+            case 'c':    
+              numero_trasportatori = atoi(optarg);
+              break;
+            case 't':    
+              time_to_sleep = atoi(optarg);
+              break;
+            case 'n': 
+              numero_operazioni= atoi(optarg);
+              break;
+              
+        }
+    }
+    // Creazione di processi figlio
+    for (int i = 1; i <= numero_trasportatori; ++i) {
+        Trasportatore trasp(i);
+        pid_t pid = fork();
+
+        if (pid == 0) { // Processo figlio
+            costumerProcesso(trasp,time_to_sleep,numero_operazioni);
+            exit(0); // Termina il processo figlio dopo aver completato il lavoro
+        } else if (pid < 0) { // Errore nella creazione del processo figlio
+            cerr << "Errore nella fork()" << endl;
+            exit(1);
+        }
+    }
+
+    // Attendi che tutti i processi figlio terminino prima di terminare il processo principale
+    int status;
+    pid_t pid;
+    while ((pid = wait(&status)) > 0);
+
+    return 0;
+} 
+
