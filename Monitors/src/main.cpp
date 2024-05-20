d7e91cee565fa53d4aadfe89c8539a1cece1a536
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-05-20 12:12:22 +0200

    Aggiunto

---
+++ Monitors/src/main.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int main(int argc,char *argv[]){
+	
+    if (argc != 2) {
+        cerr << "Uso: " << argv[0] << " --monitor <a|f|p|s>" << endl;
+        return 1;
+    }
+
+    string flag = argv[1];
+
+    if (flag == "--monitor=a") {
+        monitorAttivita();
+    } else if (flag == "--monitor=f") {
+        monitorFunzionalita();
+    } else if (flag == "--monitor=p") {
+        monitorPrestazioni();
+    } else if (flag == "--monitor=s") {
+        monitorScalabilita();
+    }else {
+        cerr << "Flag non riconosciuta. Usa --funzione <a|f|p|s>" << endl;
+        return 1;
+    }
+
+    return 0;
+}
